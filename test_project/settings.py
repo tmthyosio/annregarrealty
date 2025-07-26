27ca6d3b3d848cb042ab2be72c7ee4c7e3c2d1dd
Tim Osio <tim@ludlowpg.com>
2025-07-26 14:51:51 +0800

    Switch to PostgreSQL

--- test_project/settings.py
+++ test_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=os.environ.get('postgresql://annregar_db_user:zhreeLmkJnjLGGiZztJCFSXlbCTKn7un@dpg-d227k33e5dus739bhpbg-a/annregar_db'))
     }
 }
 
