f8575825cceb1df6ed61c2fec054eba16045a7f3
Tim Osio <tim@ludlowpg.com>
2025-07-29 11:27:49 +0800

    Auto-create superuser on deploy

--- create_superuser.py
+++ create_superuser.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass123')
+username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'ANNREGAR')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'annregar@email.com')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'ANNREGAR123')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
